0b96855895897dcdf40e9eb9da2a7368f747873e
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-09-17 22:05:07 +0200

    :construction: Add modal component

---
+++ components/AppModal.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="fixed inset-0 bg-slate-950 bg-opacity-80 flex items-center justify-center px-4 lg:px-8">
+    <div class="bg-slate-900 rounded-xl p-6 shadow-2xl max-w-md lg:max-w-lg w-full">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
