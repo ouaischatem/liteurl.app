b6f9a8463cd80d625583d758b7054382b1a4cc93
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-09-17 22:04:49 +0200

    :construction: Add icon components

---
+++ components/icons/AppErrorIcon.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex justify-center mb-4">
+    <div class="bg-red-500 rounded-full p-3">
+      <svg class="h-12 w-12 lg:h-16 lg:w-16 text-white" fill="none" stroke="currentColor"
+           stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/icons/AppSuccessIcon.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex justify-center mb-4">
+    <div class="bg-green-500 rounded-full p-3">
+      <svg class="h-12 w-12 lg:h-16 lg:w-16 text-white" fill="none" stroke="currentColor"
+           stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5 13l4 4L19 7" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
