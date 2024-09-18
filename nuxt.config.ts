eca07616f72ebdccc2ef22da92fd4b18e7398919
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-09-18 21:55:32 +0200

    :mag: Add lang attribute to HTML for improved SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
     app: {
         head: {
+            htmlAttrs: {
+                lang: 'en',
+            },
             title: 'LiteURL - Shorten & Track URLs',
             meta: [
                 {
