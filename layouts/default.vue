e4112d958557b45ce40b561a8b7757f798b297a0
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-09-19 12:40:00 +0200

    :construction: update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,7 @@ import { SpeedInsights } from '@vercel/speed-insights/vue';
 
 <template>
   <SpeedInsights />
-  <slot/>
+  <div class="min-h-screen bg-slate-950 flex flex-col justify-center gap-y-7 p-4 lg:p-8">
+    <slot/>
+  </div>
 </template>
