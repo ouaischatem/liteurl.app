c4c60238ef640d675cbc9d09747a38c40703b9ff
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-09-23 20:20:43 +0200

    :chart_with_upwards_trend: Start implementing analytics dashboard

--- pages/analytics/[short_id].vue
+++ pages/analytics/[short_id].vue
@@ -1,11 +1,66 @@
 <script lang="ts" setup>
+import type { AnalyticsData, UsageCount } from "~/types/analytics";
+import AppErrorIcon from "~/components/icons/AppErrorIcon.vue";
+
 const route = useRoute();
+const shortId = route.params.short_id as string;
+const data = ref<AnalyticsData>();
+const error = ref(false);
+const osData = ref<UsageCount[]>([]);
+const browserData = ref<UsageCount[]>([]);
+
+const generateDataCount = (key: 'os' | 'browser') => {
+  return Object.entries(
+      data.value?.details.reduce<Record<string, number>>((acc, detail) => {
+        const value = detail[key];
+        acc[value] = (acc[value] || 0) + 1;
+        return acc;
+      }, {}) || {}
+  ).map(([name, count]) => ({ name, count }));
+};
+
+onMounted(async () => {
+  const response = await $fetch(`/api/analytics/${shortId}`) as AnalyticsData;
+  if (response.original_url) {
+    data.value = response;
+
+    osData.value = generateDataCount('os');
+    browserData.value = generateDataCount('browser');
+    return;
+  }
+  error.value = true;
+});
+
+const openAnalytics = () => {
+  navigateTo(`/analytics/`);
+};
 </script>
 
 <template>
-  <h1 class="text-center text-white uppercase font-semibold text-xl ml-3">
-    Dashboard of {{route.params.short_id}}
-  </h1>
+  <AppModal :is-visible="error">
+    <AppErrorIcon/>
+    <h2 class="text-xl sm:text-2xl lg:text-3xl font-extrabold text-white mb-2 text-center">Oops, something went wrong!</h2>
+    <p class="text-gray-400 mb-6 text-center">
+      We couldn't find any information about your link. It might be incorrect or has been deleted. Please check the URL and try again.
+    </p>
+    <button class="w-full bg-red-500 hover:bg-red-600 text-white py-2 rounded-lg text-sm font-semibold transition-colors uppercase"
+            @click="openAnalytics">
+      Back
+    </button>
+  </AppModal>
+
+  <div>
+    <div class="flex md:flex-row flex-col gap-y-5 gap-x-3 items-center justify-center md:p-10">
+      <AppCard
+          title="Browsers"
+          :data="browserData"
+      />
+      <AppCard
+          title="Operating Systems"
+          :data="osData"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
